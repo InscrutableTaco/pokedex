d5d27757175b6a6c7def750adbcd1dc418550a4a
Jonathan Gibson <jonathan.w.d.gibson@gmail.com>
2025-06-02 14:56:55 -0700

    command map

--- main.go
+++ main.go
@@ -7,6 +7,30 @@ import (
 	"strings"
 )
 
+var commandMap map[string]cliCommand
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage: ")
+	fmt.Println("")
+	for _, cmd := range commandMap {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
 func cleanInput(text string) []string {
 	var output []string
 
@@ -23,6 +47,20 @@ func cleanInput(text string) []string {
 }
 
 func main() {
+
+	commandMap = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+
 	scn := bufio.NewScanner(os.Stdin)
 
 	for true {
@@ -32,9 +70,12 @@ func main() {
 
 		inputSlice := cleanInput(input)
 
-		if len(inputSlice) > 0 {
-			fmt.Println("Your command was:", inputSlice[0])
+		cmd, ok := commandMap[inputSlice[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
 		}
+		cmd.callback()
 	}
 
 }
